a67c282e8e3156c3c4a263f0279d601417d6b81d
sangik-song <sangik-song@192.168.0.20>
2023-02-10 15:17:31 +0900

    song(15:17)

--- test/src/test_1/Service/TicketServiceImpl.java
+++ test/src/test_1/Service/TicketServiceImpl.java
@@ -1,9 +1,18 @@
 package test_1.Service;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 import test_1.View.*;
 import test_1.DAO.*;
 
@@ -19,10 +28,38 @@ public class TicketServiceImpl implements TicketService{
 	@Override
 	public void printTicket(Parent root) {
 		// TODO Auto-generated method stub
-		Label title = (Label)root.lookup("#confirmTitle");
+		Label confirmTitle = (Label)root.lookup("#confirmTitle");
 //		tickets = td.selectTicket(ticket.getUserId());
-//		confirmTitle.setText(ticket.getUserId() + "님의 예매 내역");
-		title.setText("님의 예매 내역");
+//		confirmTitle.setText(ticket.getUserName() + " 님의 예매 내역");
+		confirmTitle.setText("님의 예매 내역");
+		
+		TableView<Ticket> tableView = new TableView<Ticket>();
+		tableView = (TableView)root.lookup("#main");
+		
+		TableColumn<Ticket, String> userName = new TableColumn<>("userName");
+		userName.setCellValueFactory(new PropertyValueFactory<>("userName"));
+		TableColumn<Ticket, String> movieName = new TableColumn<>("movieName");
+		movieName.setCellValueFactory(new PropertyValueFactory<>("movieName"));
+		TableColumn<Ticket, String> roomNum = new TableColumn<>("roomNum");
+		roomNum.setCellValueFactory(new PropertyValueFactory<>("roomNum"));
+		TableColumn<Ticket, String> day = new TableColumn<>("day");
+		day.setCellValueFactory(new PropertyValueFactory<>("day"));
+		TableColumn<Ticket, String> seatNum = new TableColumn<>("seatNum");
+		seatNum.setCellValueFactory(new PropertyValueFactory<>("seatNum"));
+		TableColumn<Ticket, Integer> cost = new TableColumn<>("cost");
+		cost.setCellValueFactory(new PropertyValueFactory<>("cost"));
+		TableColumn<Ticket, Integer> reserveDate = new TableColumn<>("reserveDate");
+		reserveDate.setCellValueFactory(new PropertyValueFactory<>("reserveDate"));
+		
+		tableView.getColumns().addAll(userName, movieName,roomNum,day,seatNum,cost,reserveDate);
+		
+		List<Ticket> ticketList = td.selectTicket(ticket.getUserId());
+		ObservableList<Ticket> data = 
+				FXCollections.observableArrayList(ticketList);
+		tableView.setItems(data);
+		
+		
+		
 	}
 
 }
