ea5c1c14a8c0c52122a06d37e641531adb3b3073
KHJ <KHJ@DESKTOP-EBI6QL6>
2023-02-14 16:56:17 +0900

    dd

--- test/src/test_1/Service/LoginServiceImpl.java
+++ test/src/test_1/Service/LoginServiceImpl.java
@@ -2,9 +2,11 @@ package test_1.Service;
 
 import java.io.IOException;
 
+import eum.movie.SelectMovieController;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -40,16 +42,9 @@ public class LoginServiceImpl implements LoginService{
 			cs.errorMsg("로그인", "로그인 결과", "로그인에 성공하였습니다");
 			
 			// 로그인 창 닫기
-			Stage s = (Stage) root.getScene().getWindow();
-			s.close();
-			
-			//
-			//
-			//
-			//
-			//
-			//
-			//
+//			Stage s = (Stage) root.getScene().getWindow();
+//			s.close();
+
 			//
 			//
 			//
@@ -57,6 +52,32 @@ public class LoginServiceImpl implements LoginService{
 			//
 			//
 			// 로그인 성공 시 영화 예매 페이지로 넘어가도록 추후 작업 필요
+			
+			Stage select = (Stage) root.getScene().getWindow();
+			
+			FXMLLoader loader = new FXMLLoader(
+					getClass().getResource("../../SelectMovie_1_Date.fxml")); //경로 수정
+			
+			Parent selectDate = null;
+			try {
+				selectDate = loader.load();
+				select.setScene(new Scene(selectDate));
+				
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			SelectMovieController ctrl = loader.getController();
+			ctrl.setSelectDate(selectDate);
+			
+			select.setTitle("상영 날짜 선택");
+			select.show();
+			
+			// 상영날짜 선택 콤보박스 내용 입력 (로그인 이후에 넣기)
+			ComboBox<String> cmbDate = (ComboBox<String>) selectDate.lookup("#cmbDate");
+			cmbDate.getItems().addAll("2월 15일","2월 16일","2월 17일","2월 18일","2월 19일");
 			
 		} else {
 			cs.errorMsg("로그인", "로그인 결과", "아이디/패스워드가 일치하지 않습니다");
