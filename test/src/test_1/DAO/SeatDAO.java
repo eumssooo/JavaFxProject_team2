a2f3ac5c2208ad3fd3d579013bf40ae47540f993
mamma <mamma@DESKTOP-N94SCU9>
2023-02-17 10:16:50 +0900

    10:16

--- test/src/test_1/DAO/SeatDAO.java
+++ test/src/test_1/DAO/SeatDAO.java
@@ -6,10 +6,7 @@ import test_1.View.Seat;
 import test_1.View.selData;
 
 public interface SeatDAO {
-	public ArrayList<Seat> selectSeat(String userId);	
-	public void useSeat(Seat s);						
-	public void cancelseat(Seat s);					
-						
+
 	public void nextPageSeat(selData sd);
 	public String seatCheck(selData sd);
 }
